731ce3de09b8457e0d8a6b1c9cddb47cfba0f52e
Nehal Damor <nehaldamor77@gmail.com>
2026-05-08 11:23:39 +0530

    Day 18 DSA

--- PracticeDsa.js
+++ PracticeDsa.js
@@ -1,7 +1,28 @@
-let str="42068";
-for(let i=str.length-1;i>=0;i--){
-    if(str.charAt(i)%2!==0){
-          console.log(str.slice(0,i+1))
-          break
-    };
+let n=6;
+let c=3;
+
+function generateorw(n,c){
+    let res=1;
+    for(let i=0;i<c;i++){
+        res=res*(n-i);
+        res=res/(i+1); 
+       }
+       return res;
+} 
+function generatenrow(n){
+    let ans=[];
+    let res=1;
+    ans.push(res);
+    for(let i=1;i<n;i++){
+        res=res*(n-i);
+        res/=i;
+        ans.push(res);
+    }
+    return ans;
 }
+generatenrow(6);
+let ans=[];
+for(let i=1;i<=n;i++){
+    ans.push(generatenrow(i));
+}
+console.log(ans);
